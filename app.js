c56095ae8e677d94e1adc99253a928505bb8579d
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-11 10:01:33 +0200

    Update data with PATCH

--- app.js
+++ app.js
@@ -76,3 +76,24 @@ app.get('/api/v1/tours/:id', (req, res) => {
      })
   }
 })
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// UPDATE DATA WITH PATCH
+app.patch('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1
+    const tour = tours.find((el) => el.id === id)
+  if(!tour) {
+     return res.status(404).json({
+       status: 'fail',
+       message: 'Invalid ID'
+     })
+
+  } else {
+  res.status(200).json({
+    status:'success',
+    data: {
+      tour: '<Updated tour here....>'
+    }
+  })
+}
+})
