9bc995c5fed18e90728b2d5860e7210152cfc228
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-11 13:53:52 +0200

    Create user routes setup

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { create } = require('domain');
 const express = require('express');
 const app = express();
 const fs = require('fs');
@@ -136,4 +137,114 @@ app
 .delete(deleteTour)
 .patch(patchTour)
 
+///////////////////// 
+//USERS
+///////////////////// 
 
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    message: 'This route is not design yet',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  })
+}
+
+const getUser = (req, res) => {
+  const id = req.params.id * 1
+  const tour = tours.find((el) => el.id === id)
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    })
+  } else {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    })
+  }
+}
+
+const deleteUser = (req, res) => {
+  const id = req.params.id * 1
+  const tour = tours.find((el) => el.id === id)
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    })
+  } else {
+    res.status(204).json({
+      status: 'success',
+      data: {
+        tour: 'null',
+      },
+    })
+  }
+}
+const createUser = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign(
+    {
+      id: newId,
+    },
+    req.body
+  )
+  tours.push(newTour)
+
+  // To post, we need to write the file, but always as ASYNC because we are inside a callback function!!
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        res.status(500).json({
+          status: 'error',
+          message: 'Error writing data to file',
+        })
+      } else {
+        res.status(201).json({
+          status: 'success',
+          data: {
+            tour: newTour,
+          },
+        })
+        console.log(res)
+      }
+    }
+  )
+}
+
+const patchUser = (req, res) => {
+  const id = req.params.id * 1
+  const tour = tours.find((el) => el.id === id)
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    })
+  } else {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: '<Updated tour here....>',
+      },
+    })
+  }
+}
+
+app
+.route('/api/v1/users')
+.get(getAllUsers)
+.post(createUser)
+
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .delete(deleteUser)
+  .patch(patchUser)
