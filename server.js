d2d84141aabb33d215e48fcb4f5c45f023d1ceb8
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-14 17:47:25 +0200

    Delete our code to use database instead of files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE;
@@ -14,43 +14,8 @@ mongoose
 .then(con => {
   console.log('DB CONNEXION SUSCESFULL');
 });
-// Create schema of tours
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
 
-// Create a model (always in Upeercase)
 
-const Tour = mongoose.model('Tour', tourSchema);
-
-// We create the tour
-const testTour = new Tour({
-  name: 'TLOLOLO',
-  rating: 4.7,
-  price: 497,
-});
-
-// We save our new tour.
-testTour
-.save()
-.then(doc => {
-  console.log(doc)
-})
-.catch(err => {
-  console.log('ERROR 🚨', err)
-});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
