510f757edb2da60bca3a7e1a1d0143adb07f72d2
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-14 17:13:04 +0200

    Create schema a model of our document

--- server.js
+++ server.js
@@ -15,6 +15,27 @@ mongoose
   console.log(con.connections);
   console.log('DB CONNEXION SUSCESFULL');
 });
+// Create schema of tours
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4,5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  }
+});
+
+// Create a model (always in Upeercase)
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
