687718b628440aaa0cb80dc0e1757b51f1f39002
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-15 10:14:19 +0200

    Create tours from data JSON

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -74,12 +74,12 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
+exports.deleteTour = async (req, res) => {
   try {
-    const tour = await Tour.findByIdAndDelete(req.params.id);
+    await Tour.findByIdAndDelete(req.params.id);
     res.status(204).json({
       status: 'success',
-      data: null
+      data: null,
     });
   } catch (err) {
     res.status(404).json({
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,66 +1,65 @@
 // Create schema of tours
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'A tour must have a name'],
-      unique: true,
-      trim: true,
-      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
-      minlength: [10, 'A tour name must have more or equal then 10 characters'],
-      // validate: [validator.isAlpha, 'Tour name must only contain characters']
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+    trim: true,
+    maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+    minlength: [10, 'A tour name must have more or equal then 10 characters'],
+    // validate: [validator.isAlpha, 'Tour name must only contain characters']
+  },
+  slug: String,
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult',
     },
-    slug: String,
-    duration: {
-      type: Number,
-      required: [true, 'A tour must have a duration'],
-    },
-    maxGroupSize: {
-      type: Number,
-      required: [true, 'A tour must have a group size'],
-    },
-    difficulty: {
-      type: String,
-      required: [true, 'A tour must have a difficulty'],
-      enum: {
-        values: ['easy', 'medium', 'difficult'],
-        message: 'Difficulty is either: easy, medium, difficult',
-      },
-    },
-    ratingsAverage: {
-      type: Number,
-      default: 4.5,
-    },
-    ratingsQuantity: {
-      type: Number,
-      default: 0,
-    },
-    price: {
-      type: Number,
-      required: [true, 'A tour must have a price'],
-    },
-    priceDiscount: Number,
-    summary: {
-      type: String,
-      trim: true,
-      required: [true, 'A tour must have a description'],
-    },
-    description: {
-      type: String,
-      trim: true,
-    },
-    imageCover: {
-      type: String,
-      required: [true, 'A tour must have a cover image'],
-    },
-    images: [String],
-    createdAt: {
-      type: Date,
-      default: Date.now(),
-    },
-    startDates: [Date],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
   //   secretTour: {
   //     type: Boolean,
   //     default: false,
@@ -69,8 +68,7 @@ const tourSchema = new mongoose.Schema(
   // {
   //   toJSON: { virtuals: true },
   //   toObject: { virtuals: true },
-  // },
-);
+});
 
 // Create a model (always in Upeercase)
 const Tour = mongoose.model('Tour', tourSchema);
