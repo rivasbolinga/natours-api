2d5f571ccbc4f26a2115640c243649e78522da36
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-15 14:58:14 +0200

    Refactor code creting a class that have filter, sort, fields and pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,48 +1,69 @@
 const Tour = require('../models/tourModel');
 
-exports.getAllTours = async (req, res) => {
-  try {
-    // BUILDING QUERIES
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+};
 
-    //1.A) Filtering
-    const queryObj = { ...req.query };
+class APIfeatures {
+  constructor(query, querySting) {
+    this.query = query;
+    this.querySting = querySting;
+  }
+
+  filter() {
+    const queryObj = { ...this.queryString };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    // 1.B) Advanced filtering
+    // Advanced filtering
     let queryStr = JSON.stringify(queryObj);
     // We use regular expresion to replace get, gt,lte and lt for the dolar sign.
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    let query = Tour.find(JSON.parse(queryStr));
-    //2. Sorting
+    this.query.find(JSON.parse(queryStr));
+    return this;
+  }
 
-    if (req.query.sort) {
-      const sortBy = req.query.sort;
-      query = query.sort({ [sortBy]: 1 });
-      console.log('after sorting here📍', query);
+  sort() {
+    if (this.querySting.sort) {
+      const sortBy = this.querySting.sort;
+      this.query = this.query.sort({ [sortBy]: 1 });
     } else {
-      query = query.sort({ createdAt: -1 });
+      this.query = this.query.sort({ createdAt: -1 });
     }
+    return this;
+  }
 
-    //3. Field Limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join();
-      query = query.select(fields);
+  limitFields() {
+    if (this.querySting.fields) {
+      const fields = this.querySting.fields.split(',').join();
+      this.query = this.query.select(fields);
     } else {
-      query = query.select('-__v');
+      this.query = this.query.select('-__v');
     }
+    return this;
+  }
 
-    // 4. Pagination
-    if (req.query.page) {
-      const page = req.query.page * 1 || 1;
-      const limit = req.query.limit * 1 || 100;
-      const skip = (page - 1) * limit;
-      query = query.skip(skip).limit(limit);
-    } else {
-      query = query.select('-__v');
-    }
+  paginate() {
+    const page = this.querySting.page * 1 || 1;
+    const limit = this.querySting.limit * 1 || 100;
+    const skip = (page - 1) * limit;
 
-    const tours = await query;
+    this.query = this.query.skip(skip).limit(limit);
+    return this;
+  }
+}
+
+exports.getAllTours = async (req, res) => {
+  try {
+    const features = new APIfeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const tours = await features.query;
     res.status(200).json({
       status: 'success',
       results: tours.length,
