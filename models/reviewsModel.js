c687a1f1a9dae726498b65b4597bbbf584a70b93
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-27 11:12:49 +0200

    Finish with ReviewModel

--- models/reviewsModel.js
+++ models/reviewsModel.js
@@ -3,14 +3,40 @@ const slugify = require('slugify');
 // const validator = require('validator');
 
 const reviewSchema = new mongoose.Schema({
-  reviews: {
+  review: {
     type: String,
+    required: [true, 'Review can not be empty!'],
     maxlength: [400, 'A review should have less or equal than 400 characters'],
     minlength: [10, 'A review must have more or equal than 10 characters'],
   },
   ratings: {
-    type: Number, 
-    required: [true, 'A review should have a rating']
+    type: Number,
+    required: [true, 'A review should have a rating'],
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  tours: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Review must belong to a tour.'],
+  },
+  users: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Review must belong to a user'],
+  },
+  //Options for virtual properties also show up in JSOn and object outputs
+   {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
   }
-
 });
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
