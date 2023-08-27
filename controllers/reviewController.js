fa0a42141781ee0e05b23bc114917f32d4d7b5b7
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-27 11:28:28 +0200

    Create createReview function in reviewController

---
+++ controllers/reviewController.js
@@ -0,0 +1,15 @@
+const Review = require('../models/reviewModel');
+const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  const newReview = await Review.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview,
+    },
+  });
+});
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-const slugify = require('slugify');
-// const validator = require('validator');
 
 const reviewSchema = new mongoose.Schema({
   review: {
